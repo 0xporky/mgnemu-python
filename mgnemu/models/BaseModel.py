c74ce3d4561a7367903863aaabe1af113d43aa0c
Andrew Komar <0xporky@gmil.com>
2017-06-25 11:28:02 +0000

    Add base model obect to project.

---
+++ mgnemu/models/BaseModel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Base model of project.
+Contains to basic methods:
+    dumps(object_data) - writes object into json.
+    loads(json_data) - converts json into model object.
+
+"""
+
+import json
+
+
+class BaseModel():
+
+    def dumps(object_data):
+        return json.dumps(object_data)
+
+    def loads(json_data):
+        return json.loads(json_data)
