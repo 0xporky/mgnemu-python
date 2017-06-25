c00449db623b3c63559c2aa245914f2ad64f7451
Andrew Komar <0xporky@gmil.com>
2017-06-25 21:10:46 +0000

    Add check tape controller.

---
+++ mgnemu/controllers/check_tape.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*
+
+from models.check import Check
+from tempfile import gettempdir
+from json import loads
+from json import dumps
+
+
+class CheckTape():
+
+    def __init__(self):
+        pass
+
+    def __get_check_file(self):
+        tmp_dir = gettempdir()
+        return ''.join([tmp_dir, 'tape.txt'])
+
+    def __read_check_tape(self):
+        check_tape_file = self.__get_check_file()
+        f = open(check_tape_file, 'r+')
+        check_tape_json = f.read()
+        f.close()
+
+        return check_tape_json
+
+    def __write_check_tape(self, check_tape):
+        check_tape_file = self.__get_check_file()
+        f = open(check_tape_file, 'w+')
+        f.write(dumps(check_tape))
+        f.close()
+
+    def add_check(self, json_data):
+        check = Check(json_data)
+        check_tape_json = self.__read_check_tape()
+        if len(check_tape_json) == 0:
+            check_tape = []
+        else:
+            check_tape = loads(check_tape_json)
+
+        check_tape.append(check.dumps())
+        self.__write_check_tape(check_tape)
+
+    @property
+    def check_tape(self):
+        check_tape_json = self.__read_check_tape()
+        if len(check_tape_json) == 0:
+            check_tape = []
+            return dumps(check_tape)
+
+        return check_tape_json
